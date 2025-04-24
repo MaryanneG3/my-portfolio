bd8d23df8f4972e65feb8c7d638702359295b877
Maryanne G <maryanneg@missionreadyhq.com>
2025-04-24 17:29:16 +1200

    feat: update font styles and add custom scrollbar to improve UI aesthetics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,25 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
+import { DM_Sans, Space_Grotesk, Space_Mono } from "next/font/google";
 import "./globals.css";
 
 const dmSansFont = DM_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
   variable: "--font-dm-sans",
+  display: "swap",
+});
+
+const spaceGroteskFont = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-space-grotesk",
+  display: "swap",
+});
+
+const spaceMonoFont = Space_Mono({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-space-mono",
   display: "swap",
 });
 
@@ -21,7 +35,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${dmSansFont.variable} antialiased`}>{children}</body>
+      <body
+        className={`${dmSansFont.variable} ${spaceMonoFont.variable} ${spaceGroteskFont.variable} antialiased w-screen h-screen max-w-screen max-h-screen`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
