b3c4feb63e5fb154264516462640336f87439eb3
Maryanne G <maryanneg@missionreadyhq.com>
2025-04-24 17:30:20 +1200

    feat: add ProfileCard component and personal details for dynamic rendering

---
+++ src/lib/personalDetails.ts
@@ -0,0 +1,21 @@
+export const personalDetails: {
+  name: string;
+  title: string;
+  description: string;
+  profilePicture: string;
+  email: string;
+  phone: string;
+  location: string;
+  github: string;
+  linkedin: string;
+} = {
+  name: "Maryanne Galo",
+  title: "Full Stack Developer",
+  description: `I'm Maryanne, a passionate full-stack developer with a knack for creating dynamic and responsive web applications. I thrive on challenges and love to learn new technologies.`,
+  profilePicture: "profile-image.jpg",
+  email: "maryanne_galo@outlook.com",
+  phone: "+64 027 378 5934",
+  location: "Auckland, New Zealand",
+  github: "https://github.com/MaryanneG3/",
+  linkedin: "https://www.linkedin.com/in/maryanne-galo-704501324",
+};
