9cbf69c23a55f1f4e8565022202379b33508fb44
Maryanne G <maryanneg@missionreadyhq.com>
2025-05-06 12:21:12 +1200

    refactor: enhance styling of Projects component for improved visual consistency and user interaction

--- src/app/my-projects/page.tsx
+++ src/app/my-projects/page.tsx
@@ -31,11 +31,11 @@ function Projects() {
 
   return (
     <div className="h-full w-full bg-[url('/images/background/ocean-bg-light.png')] bg-fill bg-center bg-cover bg-no-repeat lg:bg-none">
-      <div className="relative z-10 flex flex-col lg:flex-row justify-between items-center h-full w-full bg-gradient-to-b from-purple-50/20 to-purple-500/20 lg:bg-none">
+      <div className="relative z-10 flex flex-col lg:flex-row justify-between items-center h-full w-full bg-gradient-to-b from-purple-50/80 to-purple-500/30 lg:bg-none">
         <Sidebar />
 
         <motion.div
-          className="flex flex-col justify-center md:justify-center lg:justify-start items-center h-[92%] lg:h-full w-full lg:w-[80%] p-4"
+          className="flex flex-col justify-center md:justify-center lg:justify-start items-center h-[92%] lg:h-full w-full lg:w-[80%] p-4 lg:py-0"
           initial={{ opacity: 0, x: 50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1, ease: "easeOut" }}
@@ -50,7 +50,7 @@ function Projects() {
               </div>
 
               {/* Client Description */}
-              <div className="w-full h-[85%] flex flex-col justify-between gap-5 md:gap-0 py-4 mt-2 border-y-2 border-purple-900/20">
+              <div className="w-full h-[85%] flex flex-col justify-evenly gap-5 md:gap-0 py-4 mt-2 lg:mt-4 border-y-2 border-purple-900/20">
                 <div className="flex flex-col gap-2 w-full">
                   <h2 className="text-lg md:text-2xl lg:text-3xl font-semibold">
                     {currentClient.name}
@@ -68,7 +68,7 @@ function Projects() {
                       href={`/my-projects/${project.href}`}
                       className="hover:cursor-pointer"
                     >
-                      <div className="w-full flex-shrink-0 min-w-[15rem] md:w-[24rem] md:min-w-[24rem] lg:w-[28rem] lg:min-w-[28rem] p-4 flex flex-col gap-4 rounded-3xl text-purple-950 hover:shadow-xl hover:shadow-pink-200/20 hover:bg-gradient-to-b hover:from-purple-900/10 hover:to-yellow-100/70 bg-purple-900/20 transition">
+                      <div className="w-full flex-shrink-0 min-w-[15rem] md:w-[24rem] md:min-w-[24rem] lg:w-[28rem] lg:min-w-[28rem] p-4 flex flex-col gap-4 rounded-3xl text-purple-950 hover:shadow-xl hover:shadow-pink-200/20 hover:bg-gradient-to-b hover:from-purple-700/30 hover:to-yellow-100/80 bg-purple-950/20 lg:bg-purple-200 transition">
                         <div>
                           <h3 className="text-sm md:text-lg lg:text-2xl font-bold">
                             {project.client}
@@ -103,13 +103,13 @@ function Projects() {
               <div className="flex flex-row justify-between w-full gap-4 mt-3">
                 <button
                   onClick={handlePrevious}
-                  className="px-5 py-2 lg:py-3 text-sm rounded hover:cursor-pointer bg-yellow-50 hover:bg-yellow-100"
+                  className="px-5 py-2 lg:py-3 text-sm rounded hover:cursor-pointer border-2 border-purple-200 bg-purple-100 hover:bg-purple-600/20"
                 >
                   Previous
                 </button>
                 <button
                   onClick={handleNext}
-                  className="px-5 py-2 lg:py-3 text-white text-sm rounded hover:cursor-pointer bg-purple-500 hover:bg-purple-600"
+                  className="px-5 py-2 lg:py-3 text-sm rounded hover:cursor-pointer border-2 border-purple-200 bg-purple-100 hover:bg-purple-600/20"
                 >
                   Next
                 </button>
