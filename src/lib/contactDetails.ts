e2a91d55c391c69ddea96afcde092080e56dd260
Maryanne G <maryanneg@missionreadyhq.com>
2025-05-08 11:27:48 +1200

    feat: add contact details component with phone, email, GitHub, and LinkedIn information

---
+++ src/lib/contactDetails.ts
@@ -0,0 +1,21 @@
+export const contactContent = {
+  title: "My Contact Details",
+  content: [
+    {
+      label: "Phone number",
+      value: "+64 027 378 5934",
+    },
+    {
+      label: "Send me an email",
+      value: "maryanne_galo@outlook.com",
+    },
+    {
+      label: "GitHub",
+      value: "https://github.com/MaryanneG3",
+    },
+    {
+      label: "LinkedIn",
+      value: "https://www.linkedin.com/in/maryanne-galo-704501324",
+    },
+  ],
+};
